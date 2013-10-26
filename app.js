3e974e7e514c2555ec4cde874b075bdd4befc1bb
Nick Doiron <ndoiron@mapmeld.com>
2013-10-26 12:48:13 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var request = require('request');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+
+app.get('/coin', function(req, res) {
+  var requestOptions = {
+    'uri': 'http://www.quandl.com/api/v1/datasets/BITCOIN/MTGOXUSD.json?&trim_start=2013-10-14'
+  };
+  request(requestOptions, function (err, response, b) {
+    res.json( JSON.parse( b ) );
+  });
+});
+
+var port = process.env.PORT || 3000;
+app.configure(function() {
+    app.use(express.compress());
+    app.use('/static', express.static(__dirname + '/static'));
+}).listen(port);
