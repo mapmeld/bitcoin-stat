217f65696d8e518f47d0118b581eb67f4055a625
Nick Doiron <ndoiron@mapmeld.com>
2013-12-27 20:28:02 -0500

    return blank from Dogecoin API if error

--- app.js
+++ app.js
@@ -58,7 +58,13 @@ app.get('/doge', function(req, res) {
     if(err){
       return res.json([ ]);
     }
-    var prices = JSON.parse( b );
+    var prices;
+    try{
+      prices = JSON.parse( b );
+    }
+    catch(e){
+      return res.json([ ]);
+    }
     var latestPrice = prices.trades[0].rate * 1.0;
     var dt = prices.trades[0].created * 1000;
     priceModel.find({}).sort('-date').exec(function(err, results) {
